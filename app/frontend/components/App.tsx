5da25fbab71dfadcb1d8aa7dab55d8b523889ecf
Anel Danza <aneldanza23@gmail.com>
2024-06-18 19:04:19 -0400

    add tsconfig and tsconfig.node files

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,24 +1,23 @@
-import React from "react"
-import { useState } from "react"
+// import { useState } from "react"
 
 export const App = () => {
-    const [videos, setVideos] = useState<{}>()
-    return(
-        <div className="flex-col">
-        <nav className="flex bg-slate-200 p-2 justify-between">
-          <div>Logo</div>
-          <div className="border border-black rounded-2xl px-5">search bar</div>
-          <button
-            className="border border-black rounded-xl px-2 cursor-pointer"
-            // onClick={openModal}
-          >
-            Upload
-          </button>
-        </nav>
-        <main className="bg-slate-700 p-2">
-          <div className="text-white">Videos</div>
-          <div>list of videos here now</div>
-          {/* {error ? (
+  // const [videos, setVideos] = useState<{}>()
+  return (
+    <div className="flex-col">
+      <nav className="flex bg-slate-200 p-2 justify-between">
+        <div>Logo</div>
+        <div className="border border-black rounded-2xl px-5">search bar</div>
+        <button
+          className="border border-black rounded-xl px-2 cursor-pointer"
+          // onClick={openModal}
+        >
+          Upload
+        </button>
+      </nav>
+      <main className="bg-slate-700 p-2">
+        <div className="text-white">Videos</div>
+        <div className="text-blue-200">list of videos here now...</div>
+        {/* {error ? (
             <div>there was an error!</div>
           ) : isLoading ? (
             <div>Loading...</div>
@@ -29,7 +28,7 @@ export const App = () => {
               })}
             </ul>
           ) : null} */}
-        </main>
-      </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
