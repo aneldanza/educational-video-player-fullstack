eabceec6d350a84cb9321692f07cfe6d6d56513b
Anel Danza <aneldanza23@gmail.com>
2024-06-18 17:37:02 -0400

    import and configure windi css

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,7 +1,33 @@
 import React from "react"
+import { useState } from "react"
 
 export const App = () => {
+    const [videos, setVideos] = useState<{}>()
     return(
-        <div>app</div>
+        <div className="flex-col">
+        <nav className="flex bg-slate-200 p-2 justify-between">
+          <div>Logo</div>
+          <div className="border border-black rounded-2xl px-5">search bar</div>
+          <button
+            className="border border-black rounded-xl px-2 cursor-pointer"
+            // onClick={openModal}
+          >
+            Upload
+          </button>
+        </nav>
+        <main className="bg-slate-700 p-2">
+          {/* {error ? (
+            <div>there was an error!</div>
+          ) : isLoading ? (
+            <div>Loading...</div>
+          ) : data ? (
+            <ul className="flex-col divide-y divide-white">
+              {videos.map((video, i) => {
+                return <li key={`video-${i}`}>{video.title}</li>;
+              })}
+            </ul>
+          ) : null} */}
+        </main>
+      </div>
     )
 }
--- app/frontend/entrypoints/index.tsx
+++ app/frontend/entrypoints/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from '../components/App.tsx'
+import 'virtual:windi.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
