f95bdc4f22d6e6bd9559f9db81b801a046fffb76
Anel Danza <aneldanza23@gmail.com>
2024-06-18 21:24:14 -0400

    move fetching videos logic outside of render function

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,4 +1,7 @@
-import { useGetVideoByUserIdQuery, useUploadVideoMutation} from "../app/createVideosApi";
+import {
+  useGetVideoByUserIdQuery,
+  useUploadVideoMutation,
+} from "../app/createVideosApi";
 
 const userId = "anel_danza";
 
@@ -10,8 +13,9 @@ function csrfToken() {
 }
 
 export function App() {
-  const { data, error, isLoading } = useGetVideoByUserIdQuery(userId);
-  const [uploadVideo] = useUploadVideoMutation()
+  const { data, error, isLoading, isError, isSuccess } =
+    useGetVideoByUserIdQuery(userId);
+  const [uploadVideo] = useUploadVideoMutation();
 
   // const canSave = [title, content, userId].every(Boolean)
 
@@ -21,18 +25,31 @@ export function App() {
       description: "Inspirational Short Film",
       video_url: "https://youtu.be/sVPYIRF9RCQ?si=hBXm1yHSO00svGDN",
       user_id: "anel_danza",
-    }
+    };
 
     const token = csrfToken() || "";
 
     try {
-      await uploadVideo({video, csrfToken: token}).unwrap();
-    } catch(e) {
-      console.error('failed to upload a video');
+      await uploadVideo({ video, csrfToken: token }).unwrap();
+    } catch (e) {
+      console.error("failed to upload a video");
     }
+  };
 
-    
+  let content;
 
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (isSuccess) {
+    content = (
+      <ul className="flex-col divide-y divide-white">
+        {data.videos.map((video, i) => {
+          return <li key={`video-${i}`}>{video.title}</li>;
+        })}
+      </ul>
+    );
+  } else if (isError) {
+    content = <div>{error.toString()}</div>;
   }
 
   return (
@@ -47,19 +64,7 @@ export function App() {
           Upload
         </button>
       </nav>
-      <main className="bg-slate-700 p-2 text-white">
-        {error ? (
-          <div>there was an error!</div>
-        ) : isLoading ? (
-          <div>Loading...</div>
-        ) : data ? (
-          <ul className="flex-col divide-y divide-white">
-            {data.videos.map((video, i) => {
-              return <li key={`video-${i}`}>{video.title}</li>;
-            })}
-          </ul>
-        ) : null}
-      </main>
+      <main className="bg-slate-700 p-2 text-white">{content}</main>
     </div>
   );
 }
