99e71bdbb8438928f2d45fffbd5c9c21f76613b7
Anel Danza <aneldanza23@gmail.com>
2024-06-18 21:11:04 -0400

    add uploadNewVideo function with useUploadVideoMutation

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,7 +1,40 @@
-import { useGetVideoByUserIdQuery } from "../app/createVideosApi";
+import { useGetVideoByUserIdQuery, useUploadVideoMutation} from "../app/createVideosApi";
+
+const userId = "anel_danza";
+
+function csrfToken() {
+  const meta = document.querySelector("meta[name=csrf-token]");
+  const token = meta && meta.getAttribute("content");
+
+  return token ?? false;
+}
 
 export function App() {
-  const { data, error, isLoading } = useGetVideoByUserIdQuery("anel_danza");
+  const { data, error, isLoading } = useGetVideoByUserIdQuery(userId);
+  const [uploadVideo] = useUploadVideoMutation()
+
+  // const canSave = [title, content, userId].every(Boolean)
+
+  const uploadNewVideo = async () => {
+    const video = {
+      title: "THE SEED",
+      description: "Inspirational Short Film",
+      video_url: "https://youtu.be/sVPYIRF9RCQ?si=hBXm1yHSO00svGDN",
+      user_id: "anel_danza",
+    }
+
+    const token = csrfToken() || "";
+
+    try {
+      await uploadVideo({video, csrfToken: token}).unwrap();
+    } catch(e) {
+      console.error('failed to upload a video');
+    }
+
+    
+
+  }
+
   return (
     <div className="flex-col">
       <nav className="flex bg-slate-200 p-2 justify-between">
@@ -9,7 +42,7 @@ export function App() {
         <div className="border border-black rounded-2xl px-5">search bar</div>
         <button
           className="border border-black rounded-xl px-2 cursor-pointer"
-          // onClick={openModal}
+          onClick={uploadNewVideo}
         >
           Upload
         </button>
