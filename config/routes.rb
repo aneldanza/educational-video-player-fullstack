6d10fa83d24a0f3ab569fcb1c87c03d38268d85f
Anel Danza <aneldanza23@gmail.com>
2024-06-20 12:39:30 -0400

    add get method to retrieve a sinle video by id

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :videos, only: [:index, :create]
+      resources :videos, only: [:index, :create, :get]
     end
   end
 end
