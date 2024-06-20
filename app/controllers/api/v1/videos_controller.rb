89262f4a1f96af5296c29ad6e5960dd27ecdec8e
Anel Danza <aneldanza23@gmail.com>
2024-06-20 12:39:47 -0400

    add get method to retrieve a sinle video by id

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -4,8 +4,7 @@ class Api::V1::VideosController < ApplicationController
     response = get_all_user_videos_response(params[:user_id])
 
     if response.status == 200
-      @videos = JSON.parse(response.body)
-      render json: @videos
+      render json: response.body
     else
       render json: "COULD NOT GET VIDEOS FOR USER NAME"
     end
@@ -28,6 +27,24 @@ class Api::V1::VideosController < ApplicationController
       render json: JSON.parse(response.body)
     else
       render json: 'COULD NOT CREATE NEW VIDEO'
+    end
+  end
+
+  def get
+    puts 'RECEIVED GET VIDEO REQUEST'
+    @video_id = params[:video_id]
+
+    connection = Faraday.new(url: 'https://take-home-assessment-423502.uc.r.appspot.com/api')
+
+    response = connection.get('/videos/single') do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.params = {'video_id': @video_id}
+    end
+
+    if response.status === 200
+      render json: response.body
+    else
+      render json: {"error": "COULD NOT GET VIDEO"}
     end
   end
 
