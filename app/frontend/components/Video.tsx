33d6f5f7c26df491eb95801e60f017db221d685a
Anel Danza <aneldanza23@gmail.com>
2024-06-21 12:41:07 -0400

    display video with reactplayer and display vide data

--- app/frontend/components/Video.tsx
+++ app/frontend/components/Video.tsx
@@ -1,5 +1,6 @@
 import { useGetVideoByIdQuery } from "../app/createVideosApi";
 import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
 
 interface VideoProps {}
 
@@ -15,13 +16,33 @@ export const Video: React.FC<VideoProps> = () => {
     content = <div>Loading...</div>;
   } else if (isSuccess) {
     content = (
-      <ul className={`flex-col divide-y divide-white `}>{JSON.stringify(data, null, 2)}</ul>
+      <div className="flex flex-col justify-items-center">
+        <div className="w-full md:w-32 lg:w-56">
+          <ReactPlayer
+            url={data.video["video_url"]}
+            width="100%"
+            height="auto"
+            controls={true}
+          />
+          <div>
+            <div>{data.video.title}</div>
+            <div className="flex space-x-3">
+              <div>{data.video["user_id"]}</div>
+              <div>{data.video["created_at"]}</div>
+            </div>
+          </div>
+          <div>{data.video.description}</div>
+        </div>
+      </div>
     );
   } else if (isError) {
     content = <div>{error.toString()}</div>;
   }
 
-  return <>
-  <div>Single Video Page</div>
-  {content}</>;
+  return (
+    <>
+      <div>Single Video Page</div>
+      {content}
+    </>
+  );
 };
