2bdb167823af5043af0e1f93b94b0c8a6a298ebd
Anel Danza <aneldanza23@gmail.com>
2024-06-22 14:51:32 -0400

    style comment section

--- app/frontend/components/Comments.tsx
+++ app/frontend/components/Comments.tsx
@@ -1,4 +1,5 @@
 import { useGetCommentsByVideoIdQuery } from "../app/createVideosApi";
+import moment from "moment";
 
 interface CommentsProps {
   videoId: string;
@@ -10,8 +11,21 @@ export const Comments: React.FC<CommentsProps> = ({ videoId }) => {
   return (
     <>
       {isSuccess ? (
-        data.comments.map((comment: any) => {
-          return <div>{JSON.stringify(comment, null, 2)}</div>;
+        data.comments.map((comment, i) => {
+          const dateMoment = moment(comment["created_at"])
+          return (
+            <div className="flex flex-col space-y-2" key={`comment-${i}`}>
+              <div className="flex space-x-3">
+                <div className="w-5 h-5 rounded-full flex justify-center items-center bg-gray-300">
+                  <p>{comment["user_id"][0].toLowerCase()}</p>
+
+                </div>
+                <div className="font-semibold">{comment["user_id"].toLowerCase()}</div>
+                <div>{dateMoment.fromNow()}</div>
+              </div>
+              <div className="border p-2 ml-8 rounded-r-2xl rounded-bl-2xl bg-gray-200">{comment.content}</div>
+            </div>
+          );
         })
       ) : isLoading ? (
         <div>Loading...</div>
