ae10e4d075abe36b2b3b6b4e31f2d753e636935d
Anel Danza <aneldanza23@gmail.com>
2024-06-21 21:03:51 -0400

    Make Layout component the root component and render App and VideoCart as its children

--- app/frontend/components/Router.tsx
+++ app/frontend/components/Router.tsx
@@ -2,8 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { ErrorPage } from "./ErrorPage";
 import { VideoPage } from "./VideoPage";
+import { Layout } from "./Layout";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <App />, errorElement: <ErrorPage /> },
-  {path: "/videos/:videoId", element: <VideoPage />}
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <App />, errorElement: <ErrorPage />},
+      { path: "/videos/:videoId", element: <VideoPage /> },
+    ],
+  },
 ]);
