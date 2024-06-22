3a01b57e987535e1247403158b9e99d1dc734646
Anel Danza <aneldanza23@gmail.com>
2024-06-22 12:26:26 -0400

    move Comments inside CommentsContainer

--- app/frontend/components/Comments.tsx
+++ app/frontend/components/Comments.tsx
@@ -1,17 +1,27 @@
+import { useGetCommentsByVideoIdQuery } from "../app/createVideosApi";
+import { useParams } from "react-router-dom";
+
 interface CommentsProps {
   num: number;
 }
 
 export const Comments: React.FC<CommentsProps> = ({ num }) => {
+  const { videoId } = useParams();
+  const { data, isLoading, isSuccess, error } = useGetCommentsByVideoIdQuery(
+    videoId || ""
+  );
   return (
     <>
-    <div className="font-semibold">{`Comments ${num}`}</div>
-    {/* {comments.map(comment => {
-        return(
-            <div>{JSON.stringify(comment, null, 2)}</div>
-        )
-    })} */}
+      <div className="font-semibold">{`Comments ${num}`}</div>
+      {isSuccess ? (
+        data.comments.map((comment: any) => {
+          return <div>{JSON.stringify(comment, null, 2)}</div>;
+        })
+      ) : isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>{JSON.stringify(error, null, 2)}</div>
+      )}
     </>
-
-  )
+  );
 };
