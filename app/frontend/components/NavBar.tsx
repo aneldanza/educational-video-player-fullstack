12d7d4ac45f767cadaa87c0df06ca9de867bb2b5
Anel Danza <aneldanza23@gmail.com>
2024-06-22 11:12:08 -0400

    navigate to home page on logo click

--- app/frontend/components/NavBar.tsx
+++ app/frontend/components/NavBar.tsx
@@ -2,6 +2,7 @@ import {
   useUploadVideoMutation,
   useGetImagePathsQuery,
 } from "../app/createVideosApi";
+import { Link } from "react-router-dom";
 
 function csrfToken() {
   const meta = document.querySelector("meta[name=csrf-token]");
@@ -44,7 +45,7 @@ export const NavBar = () => {
         search bar
       </div>
 
-      <img src={data && data.logoColor} className="w-56" />
+      <Link to={'/'}><img src={data && data.logoColor} className="w-56" /></Link>
 
       <div
         className="border border-carribean text-white rounded-3xl py-2 px-6 cursor-pointer self-center bg-carribean"
