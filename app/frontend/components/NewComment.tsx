df1236b3fc3620ce3e627d4545987ae5db22c241
Anel Danza <aneldanza23@gmail.com>
2024-06-22 13:19:50 -0400

    set input value in component state

--- app/frontend/components/NewComment.tsx
+++ app/frontend/components/NewComment.tsx
@@ -1,20 +1,27 @@
 import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import { csrfToken } from "../utils";
 
 export const NewComment = () => {
   const [focused, setFocused] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("")
 
-  const handleInputHighlight = () => {
+  const handleInputHighlight = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFocused(true)
+    setValue(e.target.value)
+  }
+
+  const handleComment = () => {
+
   }
 
   return (
     <div className="flex flex-row space-x-4">
       <div className={`w-full border px-4 py-2 rounded-3xl self-center flex ${focused ? 'border-black' : 'border-gray-300'}`}>
         <ChatBubbleLeftEllipsisIcon className="w-5 mr-2" />
-        <input className="outline-transparent" onChange={handleInputHighlight}/>
+        <input className="outline-transparent" onChange={handleInputHighlight} value={value} />
       </div>
-      <div>Comment</div>
+      <div className="primary-btn">Comment</div>
     </div>
   );
 };
