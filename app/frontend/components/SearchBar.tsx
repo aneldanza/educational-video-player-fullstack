776f24219296419a33cde4a046fb15e81a85be7f
Anel Danza <aneldanza23@gmail.com>
2024-06-23 16:51:03 -0400

    style the select bar to look like a search bar

--- app/frontend/components/SearchBar.tsx
+++ app/frontend/components/SearchBar.tsx
@@ -1,5 +1,6 @@
-import Select from "react-select";
+import Select, { components } from "react-select";
 import { useGetVideosByUserIdQuery } from "../app/createVideosApi";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 // type VideoOption = {
 //   label: string;
@@ -8,11 +9,34 @@ import { useGetVideosByUserIdQuery } from "../app/createVideosApi";
 
 const userId = "anel_danza";
 
+const customStyles = {
+    control: (base: any) => ({
+      ...base,
+      flexDirection: 'row-reverse',
+      minWidth: '300px',
+    }),
+    clearIndicator: (base: any) => ({
+        ...base,
+        position: 'absolute',
+        right: 0
+    })
+  }
+
+const DropdownIndicator = (props: any) => {
+  return (
+    components.DropdownIndicator && (
+      <components.DropdownIndicator {...props}>
+        <MagnifyingGlassIcon className="w-3" />
+      </components.DropdownIndicator>
+    )
+  );
+};
+
 export const SearchBar: React.FC = () => {
   const { data, isSuccess } = useGetVideosByUserIdQuery(userId);
 
   return (
-    <div>
+    <div className="flex w-1/3">
       <Select
         options={
           isSuccess
@@ -23,6 +47,11 @@ export const SearchBar: React.FC = () => {
         }
         isClearable={true}
         isSearchable={true}
+        components={{
+          IndicatorSeparator: () => null,
+          DropdownIndicator,
+        }}
+        styles={customStyles}
       />
     </div>
   );
