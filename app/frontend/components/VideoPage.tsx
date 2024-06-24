db707e0e8cfc42be11ad3f3dbe4f0190f08c9762
Anel Danza <aneldanza23@gmail.com>
2024-06-24 01:56:46 -0400

    Add Spinner

--- app/frontend/components/VideoPage.tsx
+++ app/frontend/components/VideoPage.tsx
@@ -5,56 +5,58 @@ import ReactPlayer from "react-player";
 import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
 import { VideosSideBar } from "./VideosSideBar";
 import { CommentsContainer } from "./CommentsContainer";
+import { Spinner } from "./Spinner";
 
 export const VideoPage: React.FC = () => {
   const { videoId } = useParams();
-  const { data, isLoading, isError, isSuccess, error } = useGetVideoByIdQuery(
+  const { data, isLoading, isError, isSuccess } = useGetVideoByIdQuery(
     videoId || ""
   );
 
-  let content;
-
-  if (isLoading) {
-    content = <div>Loading...</div>;
-  } else if (isSuccess) {
-    const dateMoment = moment(data.video["created_at"]);
-    content = (
-      <div className="flex lg:flex-row lg:space-x-5 md:flex-row md:space-x-4 flex-col">
-        <div className="w-full lg:basis-2/3 md:basis-2/3 h-full flex flex-col justify-items-center  font-body h-viewport space-y-3 ">
-          <div className={"lg:h-130 md:h-96 h-56"}>
-            <ReactPlayer
-              url={data.video["video_url"]}
-              controls={true}
-              width="100%"
-              height="100%"
-            />
-          </div>
-          <div className="font-bold text-xl">{data.video.title}</div>
-          <div className="flex space-x-3 text-sm">
-            <div>{data.video["user_id"]}</div>
-            <div>
-              <span>{`Uploaded ${dateMoment.format("MMMM Do YYYY")}`}</span>
+  return (
+    <div className="flex lg:flex-row lg:space-x-5 md:flex-row md:space-x-4 flex-col">
+      {isLoading ? (
+        <div className="text-center mt-30">
+          <Spinner size={"20"} />
+        </div>
+      ) : isError ? (
+        <div className="text-center font-bold text-lg mt-50">Unknown Error</div>
+      ) : (
+        isSuccess && (
+          <div className="w-full lg:basis-2/3 md:basis-2/3 h-full flex flex-col justify-items-center  font-body h-viewport space-y-3 ">
+            <div className={"lg:h-130 md:h-96 h-56"}>
+              <ReactPlayer
+                url={data.video["video_url"]}
+                controls={true}
+                width="100%"
+                height="100%"
+              />
             </div>
-          </div>
-          <div className="flex space-x-1">
-            <ChatBubbleLeftIcon className="w-3 align-middle" />
-            <span className="text-xs">{`${data.video.num_comments} comments`}</span>
-          </div>
+            <div className="font-bold text-xl">{data.video.title}</div>
+            <div className="flex space-x-3 text-sm">
+              <div>{data.video["user_id"]}</div>
+              <div>
+                <span>{`Uploaded ${moment(data.video["created_at"]).format(
+                  "MMMM Do YYYY"
+                )}`}</span>
+              </div>
+            </div>
+            <div className="flex space-x-1">
+              <ChatBubbleLeftIcon className="w-3 align-middle" />
+              <span className="text-xs">{`${data.video.num_comments} comments`}</span>
+            </div>
 
-          <div className="border border-gray-100 rounded-lg p-2 bg-gray-100 text-sm">
-            {data.video.description}
-          </div>
+            <div className="border border-gray-100 rounded-lg p-2 bg-gray-100 text-sm">
+              {data.video.description}
+            </div>
 
-          <CommentsContainer num={data.video["num_comments"]} />
-        </div>
-        <div className="lg:w-500px justify-self-center">
-          <VideosSideBar />
-        </div>
+            <CommentsContainer num={data.video["num_comments"]} />
+          </div>
+        )
+      )}
+      <div className="lg:w-500px justify-self-center">
+        <VideosSideBar />
       </div>
-    );
-  } else if (isError) {
-    content = <div>{error.toString()}</div>;
-  }
-
-  return <>{content}</>;
+    </div>
+  );
 };
