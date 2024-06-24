e4bb24cd6181709091ced6d9c1c28a5e8e15c458
Anel Danza <aneldanza23@gmail.com>
2024-06-24 00:23:11 -0400

    remove current video from video list passed to Videos

--- app/frontend/components/VideosSideBar.tsx
+++ app/frontend/components/VideosSideBar.tsx
@@ -1,8 +1,10 @@
 import { useGetVideosByUserIdQuery } from "../app/createVideosApi";
 import { Videos } from "./Videos";
+import { useParams } from "react-router-dom";
 const userId = "anel_danza";
 
 export const VideosSideBar = () => {
+  const { videoId } = useParams();
   const { data, error, isLoading, isError, isSuccess, isFetching } =
     useGetVideosByUserIdQuery(userId);
 
@@ -11,12 +13,19 @@ export const VideosSideBar = () => {
   if (isLoading) {
     content = <div>Loading...</div>;
   } else if (isSuccess) {
-    content = <Videos videos={data.videos} style="grid grid-cols-1 gap-4" />;
+    const otherVideos = data.videos.filter((video) => video.id !== videoId);
+    content = <Videos videos={otherVideos} style="grid grid-cols-1 gap-4" />;
   } else if (isError) {
     content = <div>{error.toString()}</div>;
   }
 
   return (
-    <main className={`${"opacity-50" && isFetching} flex-col max-h-screen sticky top-0 h-screen overflow-y-auto`}>{content}</main>
+    <main
+      className={`${
+        "opacity-50" && isFetching
+      } flex-col max-h-screen sticky top-0 h-screen overflow-y-auto`}
+    >
+      {content}
+    </main>
   );
 };
