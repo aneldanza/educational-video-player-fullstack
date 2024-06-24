f73b318a2ed044289151a9b5e98ec46955a3feb2
Anel Danza <aneldanza23@gmail.com>
2024-06-24 01:34:36 -0400

    clean up

--- app/frontend/components/NavBar.tsx
+++ app/frontend/components/NavBar.tsx
@@ -5,41 +5,18 @@ import Modal from "react-modal";
 import { UploadVideoForm } from "./UploadVideoForm";
 import { SearchBar } from "./SearchBar";
 
-// const customStyles = {
-//   content: {
-//     top: "50%",
-//     left: "50%",
-//     right: "auto",
-//     bottom: "auto",
-//     marginRight: "-50%",
-//     transform: "translate(-50%, -50%)",
-//     width: "50%",
-//     borderRadius: "40px",
-//   },
-// };
-
 Modal.setAppElement("#root");
 
 export const NavBar = () => {
-  const { data, isError, isSuccess, error } = useGetImagePathsQuery("");
+  const { data } = useGetImagePathsQuery("");
   const [modalIsOpen, setIsOpen] = useState(false);
 
   function openModal() {
     setIsOpen(true);
   }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-  }
-
   function closeModal() {
     setIsOpen(false);
-  }
-
-  if (isError) {
-    console.log(error);
-  } else if (isSuccess) {
-    console.log(data);
   }
 
   return (
@@ -60,7 +37,6 @@ export const NavBar = () => {
 
       <Modal
         isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         className="modal-custom"
         contentLabel="Upload Video Modal"
